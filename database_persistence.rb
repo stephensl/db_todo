82d782afedb533c7958693119d981f3ea3a499c3
Lawton Stephens <stephens.lawton@gmail.com>
2023-10-14 14:39:48 -0400

    update database_persistence class

--- database_persistence.rb
+++ database_persistence.rb
@@ -33,42 +33,39 @@ class DatabasePersistence
     end 
   end 
 
-  def create_new_list(list_name)
-    # id = next_element_id(@session[:lists])
-    # @session[:lists] << { id: id, name: list_name, todos: [] }
+  def create_new_list(name)
+    sql = "INSERT INTO lists (list_name) VALUES ($1)"
+    query(sql, name)
   end 
 
   def delete_list(id)
-    # @session[:lists].reject! { |list| list[:id] == id }
+    query("DELETE FROM todos WHERE list_id = $1", id)
+    query("DELETE FROM lists WHERE id = $1", id)
   end 
 
   def update_list_name(id, new_name)
-    # list = find_list(id)
-    # list[:name] = new_name
+    sql = "UPDATE lists SET list_name = $2 WHERE id = $1"
+    query(sql, id, new_name)
   end 
 
   def create_new_todo(list_id, todo_name)
-    # list = find_list(list_id)
-    # id = next_element_id(list[:todos])
-    # list[:todos] << { id: id, name: todo_name, completed: false }
+    sql = "INSERT INTO todos (list_id, todo_name) VALUES ($1, $2)"
+    query(sql, list_id, todo_name)
   end 
 
   def delete_todo_from_list(list_id, todo_id)
-    # list = find_list(list_id)
-    # list[:todos].reject! { |todo| todo[:id] == todo_id }
+    sql = "DELETE FROM todos WHERE id = $1 AND list_id = $2"
+    query(sql, todo_id, list_id)
   end 
 
   def update_todo_status(list_id, todo_id, new_status)
-    # list = find_list(list_id)
-    # todo = list[:todos].find { |t| t[:id] == todo_id }
-    # todo[:completed] = new_status
+    sql = "UPDATE todos SET completed = $1 WHERE id = $2 AND list_id = $3"
+    query(sql, new_status, todo_id, list_id)
   end 
 
   def mark_all_todos_as_completed(list_id)
-    # list = find_list(list_id)
-    # list[:todos].each do |todo|
-    #   todo[:completed] = true
-    # end
+    sql = "UPDATE todos SET completed = true WHERE list_id = $1"
+    query(sql, list_id)
   end 
 
   private 
